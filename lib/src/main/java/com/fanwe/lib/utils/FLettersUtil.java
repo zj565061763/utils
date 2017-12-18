7f625de7a8ea0a53690021250c15f5832fffe16a
zjun <565061763@qq.com>
2017-12-18 10:40:28 +0800

    utils

--- lib/src/main/java/com/fanwe/lib/utils/FLettersUtil.java
+++ lib/src/main/java/com/fanwe/lib/utils/FLettersUtil.java
@@ -5,9 +5,9 @@ import android.text.TextUtils;
 /**
  * 拼音汉字工具类
  */
-public class FPYUtil
+public class FLettersUtil
 {
-    private static int[] mArrPinYinValue = new int[]{-20319, -20317, -20304, -20295, -20292, -20283, -20265, -20257, -20242, -20230, -20051,
+    private static final int[] ARR_PINYIN_VALUE = new int[]{-20319, -20317, -20304, -20295, -20292, -20283, -20265, -20257, -20242, -20230, -20051,
             -20036, -20032, -20026, -20002, -19990, -19986, -19982, -19976, -19805, -19784, -19775, -19774, -19763, -19756, -19751, -19746, -19741,
             -19739, -19728, -19725, -19715, -19540, -19531, -19525, -19515, -19500, -19484, -19479, -19467, -19289, -19288, -19281, -19275, -19270,
             -19263, -19261, -19249, -19243, -19242, -19238, -19235, -19227, -19224, -19218, -19212, -19038, -19023, -19018, -19006, -19003, -18996,
@@ -31,7 +31,7 @@ public class FPYUtil
             -11536, -11358, -11340, -11339, -11324, -11303, -11097, -11077, -11067, -11055, -11052, -11045, -11041, -11038, -11024, -11020, -11019,
             -11018, -11014, -10838, -10832, -10815, -10800, -10790, -10780, -10764, -10587, -10544, -10533, -10519, -10331, -10329, -10328, -10322,
             -10315, -10309, -10307, -10296, -10281, -10274, -10270, -10262, -10260, -10256, -10254};
-    public static String[] mArrPinYinString = new String[]{"a", "ai", "an", "ang", "ao", "ba", "bai", "ban", "bang", "bao", "bei", "ben", "beng",
+    public static final String[] ARR_PINYIN_STRING = new String[]{"a", "ai", "an", "ang", "ao", "ba", "bai", "ban", "bang", "bao", "bei", "ben", "beng",
             "bi", "bian", "biao", "bie", "bin", "bing", "bo", "bu", "ca", "cai", "can", "cang", "cao", "ce", "ceng", "cha", "chai", "chan", "chang",
             "chao", "che", "chen", "cheng", "chi", "chong", "chou", "chu", "chuai", "chuan", "chuang", "chui", "chun", "chuo", "ci", "cong", "cou",
             "cu", "cuan", "cui", "cun", "cuo", "da", "dai", "dan", "dang", "dao", "de", "deng", "di", "dian", "diao", "die", "ding", "diu", "dong",
@@ -102,11 +102,11 @@ public class FPYUtil
             result = String.valueOf((char) ascii);
         } else
         {
-            for (int i = (mArrPinYinValue.length - 1); i >= 0; i--)
+            for (int i = (ARR_PINYIN_VALUE.length - 1); i >= 0; i--)
             {
-                if (mArrPinYinValue[i] <= ascii)
+                if (ARR_PINYIN_VALUE[i] <= ascii)
                 {
-                    result = mArrPinYinString[i];
+                    result = ARR_PINYIN_STRING[i];
                     break;
                 }
             }
