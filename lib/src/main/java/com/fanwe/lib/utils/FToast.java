031d7e472f728ad16e504e7108395ebb09237b1a
zjun <565061763@qq.com>
2017-12-18 11:07:27 +0800

    utils

---
+++ lib/src/main/java/com/fanwe/lib/utils/FToast.java
@@ -0,0 +1,56 @@
+package com.fanwe.lib.utils;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+/**
+ * toast显示类，可以在子线程直接调用
+ */
+public class FToast
+{
+    private static Toast sToast;
+
+    public static void show(CharSequence text, Context context)
+    {
+        show(text, Toast.LENGTH_LONG, context);
+    }
+
+    public static void show(final CharSequence text, final int duration, final Context context)
+    {
+        if (Looper.myLooper() == Looper.getMainLooper())
+        {
+            showInternal(text, duration, context);
+        } else
+        {
+            new Handler(Looper.getMainLooper()).post(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    showInternal(text, duration, context);
+                }
+            });
+        }
+    }
+
+    private static void showInternal(CharSequence text, int duration, Context context)
+    {
+        if (TextUtils.isEmpty(text))
+        {
+            return;
+        }
+        if (sToast != null)
+        {
+            sToast.setText(text);
+            sToast.setDuration(duration);
+        } else
+        {
+            sToast = Toast.makeText(context, text, duration);
+        }
+        sToast.show();
+    }
+
+}
