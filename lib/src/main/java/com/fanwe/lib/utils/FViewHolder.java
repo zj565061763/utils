43163890046b8a713b8b4b7d9b68073bdb0350bd
zjun <565061763@qq.com>
2017-12-25 09:02:40 +0800

    utils

---
+++ lib/src/main/java/com/fanwe/lib/utils/FViewHolder.java
@@ -0,0 +1,29 @@
+package com.fanwe.lib.utils;
+
+import android.util.SparseArray;
+import android.view.View;
+
+public final class FViewHolder
+{
+
+    private FViewHolder()
+    {
+    }
+
+    public static <T extends View> T get(int id, View convertView)
+    {
+        SparseArray<View> holder = (SparseArray<View>) convertView.getTag();
+        if (holder == null)
+        {
+            holder = new SparseArray<>();
+            convertView.setTag(holder);
+        }
+        View childView = holder.get(id);
+        if (childView == null)
+        {
+            childView = convertView.findViewById(id);
+            holder.put(id, childView);
+        }
+        return (T) childView;
+    }
+}
