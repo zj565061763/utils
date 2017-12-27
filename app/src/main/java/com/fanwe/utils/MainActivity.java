bb7661551a337c0b42f117898d0debb4c41864f2
zjun <565061763@qq.com>
2017-12-27 09:42:22 +0800

    utils

--- app/src/main/java/com/fanwe/utils/MainActivity.java
+++ app/src/main/java/com/fanwe/utils/MainActivity.java
@@ -3,12 +3,8 @@ package com.fanwe.utils;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.fanwe.lib.utils.extend.BitmapLruCache;
-
 public class MainActivity extends AppCompatActivity
 {
-    public static final BitmapLruCache BITMAP_CACHE = new BitmapLruCache();
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
