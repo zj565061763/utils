a1afa989518d3e871e0d2ab767e97f17a6aba0fa
zjun <565061763@qq.com>
2018-01-03 16:44:52 +0800

    utils

---
+++ lib/src/main/java/com/fanwe/lib/utils/FNetUtil.java
@@ -0,0 +1,50 @@
+package com.fanwe.lib.utils;
+
+import android.text.TextUtils;
+
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.UnknownHostException;
+
+/**
+ * 网络工具类
+ */
+
+public final class FNetUtil
+{
+    private FNetUtil()
+    {
+    }
+
+    /**
+     * 域名是否可用，只能在后台线程调用
+     *
+     * @param host
+     * @return
+     */
+    public static boolean isHostAvailable(String host)
+    {
+        try
+        {
+            InetAddress inetAddress = InetAddress.getByName(host);
+            String hostAddress = inetAddress.getHostAddress();
+            return !TextUtils.isEmpty(hostAddress);
+        } catch (UnknownHostException e)
+        {
+            return false;
+        }
+    }
+
+    public static String getHost(String url)
+    {
+        try
+        {
+            URI uri = new URI(url);
+            return uri.getHost();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
